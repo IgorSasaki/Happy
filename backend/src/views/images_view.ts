7426d148106919848d5e01dee641e097c68abaa3
Igor Sasaki <igor-sasaki@hotmail.com>
2020-10-14 21:42:14 -0300

    Segundo dia da Next level Week promovido pela Rocketseat

---
+++ backend/src/views/images_view.ts
@@ -0,0 +1,13 @@
+import Images from "../models/Image";
+
+export default {
+    render(image: Images) {
+        return {
+            id: image.id,
+            url: `http://localhost:3333/uploads/${image.path}`
+        }
+    },
+    renderMany(image: Images[]){
+        return image.map(image => this.render(image))
+    }
+};
